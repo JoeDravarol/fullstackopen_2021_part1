5b731bc03c91c500aecc720a8fb5fb1d2e243fce
Joe <joedravarol@gmail.com>
2021-05-29 23:33:33 +0100

    Complete unicafe exercise step3 Extract the statistics into its own Statistics component.

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -8,15 +8,31 @@ const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const totalFeedback = good + neutral + bad
+  const averageFeedback = (good - bad) / totalFeedback
+  const positiveFeedbackPercentage = (good / totalFeedback) * 100
+
+  return (
+    <>
+      <h2>Statistics</h2>
+
+      <Feedback text='good' value={good} />
+      <Feedback text='neutral' value={neutral} />
+      <Feedback text='bad' value={bad} />
+      <Feedback text='all' value={totalFeedback} />
+
+      <p>average {averageFeedback}</p>
+      <p>positive {positiveFeedbackPercentage} %</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const totalFeedback = good + neutral + bad
-  const averageFeedback = (good - bad) / totalFeedback
-  const positiveFeedbackPercentage = (good / totalFeedback) * 100
 
   return (
     <div>
@@ -25,14 +41,11 @@ const App = () => {
       <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
       <Button text='bad' handleClick={() => setBad(bad + 1)} />
 
-      <h2>Statistics</h2>
-      <Feedback text='good' value={good} />
-      <Feedback text='neutral' value={neutral} />
-      <Feedback text='bad' value={bad} />
-      <Feedback text='all' value={totalFeedback} />
-      
-      <p>average {averageFeedback}</p>
-      <p>positive {positiveFeedbackPercentage} %</p>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }
